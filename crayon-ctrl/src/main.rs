60dfcd1674d0409ecefc166f43cb0d4e35c8e8e7
Carlos Valiente <carlos@pepelabs.net>
2020-02-12 19:56:22 +0000

    Initial commit.

--- crayon-ctrl/src/main.rs
+++ crayon-ctrl/src/main.rs
@@ -4,9 +4,34 @@ use warp::Filter;
 async fn main() {
     env_logger::init();
 
-    // GET /health => Emty 200 response.
+    // GET /health => Empty 200 response.
     let health = warp::get().and(warp::path("health").map(|| "").and(warp::path::end()));
 
-    let routes = health;
+    let links = warp::path("links").and(
+        // GET /links => JSON list of links
+        warp::get()
+            .and(warp::path::end())
+            .map(|| "[1, 2, 3]")
+            .or(
+                // GET /links/<name> => JSON object or 404.
+                warp::get().and(
+                    warp::path::param()
+                        .and(warp::path::end())
+                        .map(|name: String| format!("id: {}", name)),
+                ),
+            )
+            .or(
+                // POST /links {JSON body} => Empty response.
+                warp::post().and(warp::body::json()).map(|_body: String| ""),
+            )
+            .or(
+                // PATCH /links/<name> {JSON body} => JSON object or 404.
+                warp::patch()
+                    .and(warp::body::json())
+                    .map(|_body: String| ""),
+            ),
+    );
+
+    let routes = health.or(links);
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
