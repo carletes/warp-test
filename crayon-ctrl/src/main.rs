82f7d9fd07739644f17c88bfd00e5169ff6dbaf4
Carlos Valiente <carlos@pepelabs.net>
2020-02-12 19:03:23 +0000

    Initial commit.

--- crayon-ctrl/src/main.rs
+++ crayon-ctrl/src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    // GET /health => Emty 200 response.
+    let health = warp::get().and(warp::path("health").map(|| "").and(warp::path::end()));
+
+    let routes = health;
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
+}
