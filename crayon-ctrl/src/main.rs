321c2ee76abf8a8cad32aece87e821b462be6442
Carlos Valiente <carlos@pepelabs.net>
2020-02-12 20:19:44 +0000

    Initial commit.

--- crayon-ctrl/src/main.rs
+++ crayon-ctrl/src/main.rs
@@ -7,29 +7,31 @@ async fn main() {
     // GET /health => Empty 200 response.
     let health = warp::get().and(warp::path("health").map(|| "").and(warp::path::end()));
 
+    // GET /links => JSON list of links
+    let links_list = warp::get().and(warp::path::end()).map(|| "[1, 2, 3]");
+
+    // GET /links/<name> => JSON object or 404.
+    let links_detalil = warp::get()
+        .and(warp::path::param().and(warp::path::end()))
+        .map(|name: String| format!("id: {}", name));
+
+    // POST /links {JSON body} => Empty response.
+    let links_create = warp::post()
+        .and(warp::path::end())
+        .and(warp::body::json())
+        .map(|_body: String| "");
+
+    // PATCH /links/<name> {JSON body} => JSON object or 404.
+    let links_modify = warp::patch()
+        .and(warp::path::param().and(warp::path::end()))
+        .and(warp::body::json())
+        .map(|_name: String, _body: String| "");
+
     let links = warp::path("links").and(
-        // GET /links => JSON list of links
-        warp::get()
-            .and(warp::path::end())
-            .map(|| "[1, 2, 3]")
-            .or(
-                // GET /links/<name> => JSON object or 404.
-                warp::get().and(
-                    warp::path::param()
-                        .and(warp::path::end())
-                        .map(|name: String| format!("id: {}", name)),
-                ),
-            )
-            .or(
-                // POST /links {JSON body} => Empty response.
-                warp::post().and(warp::body::json()).map(|_body: String| ""),
-            )
-            .or(
-                // PATCH /links/<name> {JSON body} => JSON object or 404.
-                warp::patch()
-                    .and(warp::body::json())
-                    .map(|_body: String| ""),
-            ),
+        links_create
+            .or(links_detalil)
+            .or(links_list)
+            .or(links_modify),
     );
 
     let routes = health.or(links);
