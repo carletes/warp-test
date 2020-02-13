abccf05178ffa4648cb5ef9fda773019a82c46da
Carlos Valiente <carlos@pepelabs.net>
2020-02-13 11:32:02 +0000

    Initial commit.

--- crayon-ctrl/src/main.rs
+++ crayon-ctrl/src/main.rs
@@ -7,33 +7,82 @@ async fn main() {
     // GET /health => Empty 200 response.
     let health = warp::get().and(warp::path("health").map(|| "").and(warp::path::end()));
 
-    // GET /links => JSON list of links
-    let links_list = warp::get().and(warp::path::end()).map(|| "[1, 2, 3]");
-
-    // GET /links/<name> => JSON object or 404.
-    let links_detalil = warp::get()
-        .and(warp::path::param().and(warp::path::end()))
-        .map(|name: String| format!("id: {}", name));
-
     // POST /links {JSON body} => Empty response.
     let links_create = warp::post()
         .and(warp::path::end())
         .and(warp::body::json())
         .map(|_body: String| "");
 
-    // PATCH /links/<name> {JSON body} => JSON object or 404.
-    let links_modify = warp::patch()
-        .and(warp::path::param().and(warp::path::end()))
-        .and(warp::body::json())
-        .map(|_name: String, _body: String| "");
-
     let links = warp::path("links").and(
         links_create
-            .or(links_detalil)
-            .or(links_list)
-            .or(links_modify),
+            .or(links::detail())
+            .or(links::list())
+            .or(links::modify()),
     );
 
     let routes = health.or(links);
     warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
+}
+
+mod links {
+    use warp::{Filter, Rejection, Reply};
+
+    // GET /links => JSON list of links
+    pub fn list() -> impl Filter<Extract = impl Reply, Error = Rejection> + Copy {
+        warp::get().and(warp::path::end()).map(|| "[1, 2, 3]")
+    }
+
+    // GET /links/<name> => JSON object or 404.
+    pub fn detail() -> impl Filter<Extract = (String,), Error = Rejection> + Copy {
+        warp::get()
+            .and(warp::path::param().and(warp::path::end()))
+            .map(|name: String| format!("id: {}", name))
+    }
+
+    // PATCH /links/<name> {JSON body} => JSON object or 404.
+    pub fn modify() -> impl Filter<Extract = impl Reply, Error = Rejection> + Copy {
+        warp::patch()
+            .and(warp::path::param().and(warp::path::end()))
+            .and(warp::body::json())
+            .map(|_name: String, _body: String| "")
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[tokio::test]
+        async fn test_list() {
+            let f = list();
+
+            let res = warp::test::request()
+                .method("GET")
+                .path("/")
+                .reply(&f)
+                .await;
+
+            assert_eq!(res.status(), 200);
+            assert_eq!(res.body(), "[1, 2, 3]");
+        }
+
+        #[tokio::test]
+        async fn test_detail() {
+            let f = detail();
+
+            let res = warp::test::request()
+                .method("GET")
+                .path("/foo")
+                .reply(&f)
+                .await;
+            assert_eq!(res.status(), 200);
+            assert_eq!(res.body(), "id: foo");
+
+            let res = warp::test::request()
+                .method("GET")
+                .path("/foo/bar")
+                .reply(&f)
+                .await;
+            assert_eq!(res.status(), 404);
+        }
+    }
 }
