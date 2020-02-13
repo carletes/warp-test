47a70e204a53dbb2b8c577eb83e544ff267c78d1
Carlos Valiente <carlos@pepelabs.net>
2020-02-13 11:57:20 +0000

    Initial commit.

---
+++ crayon-ctrl/src/links.rs
@@ -0,0 +1,67 @@
+use warp::{Filter, Rejection, Reply};
+
+/// GET /links => JSON list of links
+pub fn list() -> impl Filter<Extract = impl Reply, Error = Rejection> + Copy {
+    warp::get().and(warp::path::end()).map(|| "[1, 2, 3]")
+}
+
+/// GET /links/<name> => JSON object or 404.
+pub fn detail() -> impl Filter<Extract = (String,), Error = Rejection> + Copy {
+    warp::get()
+        .and(warp::path::param().and(warp::path::end()))
+        .map(|name: String| format!("id: {}", name))
+}
+
+/// POST /links {JSON body} => Empty response.
+pub fn create() -> impl Filter<Extract = impl Reply, Error = Rejection> + Copy {
+    warp::post()
+        .and(warp::path::end())
+        .and(warp::body::json())
+        .map(|_body: String| "")
+}
+
+/// PATCH /links/<name> {JSON body} => JSON object or 404.
+pub fn modify() -> impl Filter<Extract = impl Reply, Error = Rejection> + Copy {
+    warp::patch()
+        .and(warp::path::param().and(warp::path::end()))
+        .and(warp::body::json())
+        .map(|_name: String, _body: String| "")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_list() {
+        let f = list();
+
+        let res = warp::test::request()
+            .method("GET")
+            .path("/")
+            .reply(&f)
+            .await;
+
+        assert_eq!(res.status(), 200);
+        assert_eq!(res.body(), "[1, 2, 3]");
+    }
+
+    #[tokio::test]
+    async fn test_detail() {
+        let f = detail();
+
+        let res = warp::test::request()
+            .method("GET")
+            .path("/foo")
+            .reply(&f)
+            .await;
+        assert_eq!(res.status(), 200);
+        assert_eq!(res.body(), "id: foo");
+
+        let res = warp::test::request()
+            .method("GET")
+            .path("/foo/bar")
+            .reply(&f)
+            .await;
+        assert_eq!(res.status(), 404);
+    }}
