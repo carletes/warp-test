3bb95ef6eef5a23de94a3eadcf8e4434dfc8f274
Carlos Valiente <carlos@pepelabs.net>
2020-02-14 16:31:21 +0000

    Initial commit

--- crayon-ctrl/src/models.rs
+++ crayon-ctrl/src/models.rs
@@ -1,3 +1,5 @@
+use nix::ifaddrs::{getifaddrs, InterfaceAddress};
+
 #[derive(Debug)]
 pub struct Interface {
     pub dev: String,
@@ -5,24 +7,66 @@ pub struct Interface {
     prefix: u8,
 }
 
-pub trait Interfaces {
-    fn add(&mut self, iface: Interface) -> Result<(), String>;
-    fn get(&self, dev: &str) -> Option<&Interface>;
-    fn list(&self) -> Vec<Interface>;
+impl Interface {
+    fn from_ifaddr(addr: InterfaceAddress) -> Interface {
+        Interface {
+            dev: addr.interface_name,
+            addr: "pepe".to_string(),
+            prefix: 42,
+        }
+    }
 }
 
-struct SysInterfaces {}
+pub trait Interfaces {
+    fn add(&mut self, iface: Interface) -> Result<(), String>;
+    fn get(&self, dev: &str) -> Option<Interface>;
+    fn list(&self) -> Result<Vec<Interface>, String>;
+}
+
+pub struct SysInterfaces {}
 
 impl Interfaces for SysInterfaces {
     fn add(&mut self, iface: Interface) -> Result<(), String> {
         Err(format!("Not implemented: add({:?})", iface))
     }
 
-    fn get(&self, dev: &str) -> Option<&Interface> {
-        None
+    fn get(&self, dev: &str) -> Option<Interface> {
+        match getifaddrs() {
+            Ok(addrs) => {
+                for addr in addrs {
+                    if addr.interface_name == dev {
+                        return Some(Interface::from_ifaddr(addr));
+                    }
+                }
+
+                None
+            }
+            Err(_) => None,
+        }
     }
 
-    fn list(&self) -> Vec<Interface> {
-        vec![]
+    fn list(&self) -> Result<Vec<Interface>, String> {
+        match getifaddrs() {
+            Ok(addrs) => {
+                let mut ret = Vec::new();
+                for addr in addrs {
+                    ret.push(Interface::from_ifaddr(addr));
+                }
+                Ok(ret)
+            }
+            Err(err) => Err(format!("getifaddrs() failed: {}", err)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Interfaces, SysInterfaces};
+
+    #[test]
+    fn all_interfaces() {
+        let system = SysInterfaces {};
+        let ifaces = system.list().unwrap();
+        assert!(!ifaces.is_empty());
     }
 }
